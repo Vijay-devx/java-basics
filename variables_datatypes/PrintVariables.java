9d1ce0b4bde42cc81cecc6903d35a2457e4acd6f
Vijay-devx <yadavvijay3107@gmail.com>
2026-02-02 18:03:41 +0530

    Added PrintVariables.java

---
+++ variables_datatypes/PrintVariables.java
@@ -0,0 +1,20 @@
+
+// Declare and Print Variable.
+
+
+public class PrintVariables 
+{
+    public static void main(String[] args)
+    {
+        int age = 22;
+        double height = 5.8;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println();
+
+        System.out.printf("Age: %d\n Height: %.2f\n Grade: %c\n Student: %b\n",age,height,grade,isStudent);
+
+    }
+    
+}
