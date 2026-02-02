e43c9d31fc5fea519a9ec6918dc6e4c803aeb5f6
Vijay-devx <yadavvijay3107@gmail.com>
2026-02-02 10:46:15 +0530

    Added ReadLine.java

---
+++ input_output/ReadLine.java
@@ -0,0 +1,18 @@
+//Reading Full Line
+
+
+import java.util.Scanner;
+
+public class ReadLine 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter a Line: ");
+        String s = sc.nextLine();                             //sc.nextLine takes lines as input, it counts spaces as well.
+
+        System.out.println("You entered: "+s);
+        sc.close();
+    }
+    
+}
