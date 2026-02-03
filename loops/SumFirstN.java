437ec8980cc9637a4b6fa9a8d13d8a688e45026f
Vijay-devx <yadavvijay3107@gmail.com>
2026-02-03 22:40:07 +0530

    added SumFirstN.java

---
+++ loops/SumFirstN.java
@@ -0,0 +1,23 @@
+
+//Sum of First N Natural Numbers
+
+import java.util.Scanner;
+
+public class SumFirstN 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter N^th term: ");
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for(int i=1; i<=n; i++)
+        {
+            sum = sum + i;
+        }
+        System.out.println("Sum = "+sum);
+        sc.close();
+    }
+    
+}
