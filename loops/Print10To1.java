d5b22ede9e5d87f3e8e5128c5d0abb18daf61d22
Vijay-devx <yadavvijay3107@gmail.com>
2026-02-03 22:22:27 +0530

    added Print10To1.java

---
+++ loops/Print10To1.java
@@ -0,0 +1,14 @@
+
+//Print Number 10 to 1
+
+public class Print10To1 
+{
+    public static void main(String[] args)
+    {
+        for(int i=10; i>0; i--)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
