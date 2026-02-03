f8d8cf3e55d2d0326760cc933271fbc922dd472f
Vijay-devx <yadavvijay3107@gmail.com>
2026-02-04 01:10:09 +0530

    modified ReverseNumber.java

--- loops/ReverseNumber.java
+++ loops/ReverseNumber.java
@@ -8,7 +8,25 @@ public class ReverseNumber
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("Please Enter a Number: ");
+        int n = sc.nextInt();
+        int reverse = 0;
+
+        if(n==0)
+        {
+            System.out.println(n);
+        }
+        else
+        {
+            while(n!=0)
+            {
+                int digits = n % 10;  //Extract Last Digit.
+                reverse = reverse*10+digits;  //attach the extracted digit
+                n = n/10;             // deleting the last digit
+            }
+            System.out.printf("Reversed Number = %d\n",reverse);
+        }
+        sc.close();
     }
 
     
